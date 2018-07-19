fd0ecdf8021619ec45e258d5e176501aed07f745
Isaac Owomugisha <archimedesisaac@gmail.com>
2018-07-19 21:32:42 +0300

    Add fetchAPI function to ride offers script

--- scripts/ride_offers.js
+++ scripts/ride_offers.js
@@ -3,6 +3,29 @@
  * and some extra stuff to make the page come to life.
  * */
 
+// This function handles all the fetching from the API and returns the json response
+function fetchAPI(url, method, headers, body, action) {
+    if (!('fetch' in window)) {
+        console.log('Fetch API not found');
+        return;
+    }
+
+    fetch(url,{
+        method: method,
+        headers: headers,
+        body: body
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (error) {
+            console.log("There's a problem: \n" + error);
+        })
+        .then(function (myJson) {
+            action(myJson);
+        });
+}
+
 
 // This function logs the user out by replacing the token with some random stuff.
 let logout = function () {
